6650dec1e05a646ec50a4d3239f802840226e40b
malucart <malucartaxo_@hotmail.com>
2021-09-11 22:08:07 -0700

    making the how is today phrase animated

--- app/src/main/java/com/example/luizaweatherapp/WelcomeActivity.java
+++ app/src/main/java/com/example/luizaweatherapp/WelcomeActivity.java
@@ -1,13 +1,18 @@
+/*
+ Welcome screen
+*/
+
 package com.example.luizaweatherapp;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.luizaweatherapp.animation.TypeWriter;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -17,12 +22,16 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         // changing color on status bar
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.mainBlue));
-        }
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.mainBlue));
+
+        // animation on textview
+        final TypeWriter tw = (TypeWriter) findViewById(R.id.tvTitle);
+        tw.setText("");
+        tw.setCharacterDelay(130);
+        tw.animateText(getString(R.string.howIsToday));
 
         Button btnGo = findViewById(R.id.btnGo);
 
